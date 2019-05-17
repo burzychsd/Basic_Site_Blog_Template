7362a4496b5bcd9b86effa15b9d427867da02cf2
burzychs <burzychs@gmail.com>
2019-05-17 23:12:16 +0200

    add Vector atom

---
+++ src/components/atoms/Vector/index.js
@@ -0,0 +1,14 @@
+// DEPENDENCIES
+import React from 'react'
+import styled from 'styled-components'
+import tw from 'tailwind.macro'
+
+const Img = styled.img`
+    ${props => props.reset ? null : tw`w-full h-auto`};
+`
+
+const Vector = ({ css, src, alt, style }) => (
+    <Img css={css} src={src} alt={alt} style={style} />
+)
+
+export default Vector
